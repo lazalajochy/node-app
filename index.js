5da2cb679e031223ffc05864ad70fa4b55190aa1
lazalajochy <lazala1401jochy@outlook.com>
2025-01-24 23:56:57 -0400

    serv

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Node Server is running. We can do it now and today..... Yay!! 🌟');
+    res.send('Node Server is running. We can do it now and today. Yay!! 🌟');
 });
 
 app.listen(3000, () => {

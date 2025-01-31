61a729e6e7409c0200d219048db75d8345dd477c
lazalajochy <lazala1401jochy@outlook.com>
2025-01-31 15:29:30 -0400

    add ip address for the db

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config()
 
 export const db = new Sequelize(process.env._POSTGRES_DB, process.env._POSTGRES_USER ,process.env._POSTGRES_PASSWORD, {
-    host:process.env._HOST,
+    host:"34.46.137.174",
     dialect:"postgres"
 })

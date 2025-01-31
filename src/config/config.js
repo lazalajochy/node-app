b927fb533c8c2883504fc82d1a3d7550a114899f
lazalajochy <lazala1401jochy@outlook.com>
2025-01-31 15:50:21 -0400

    add   db name

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config()
 
-export const db = new Sequelize( process.env._POSTGRES_USER ,process.env._POSTGRES_PASSWORD, {
+export const db = new Sequelize(process.env._POSTGRES_DB, process.env._POSTGRES_USER ,process.env._POSTGRES_PASSWORD, {
     host:"34.46.137.174",
     dialect:"postgres"
 })

f12382889d344405dd3811311da5f54046dfcbcb
lazalajochy <lazala1401jochy@outlook.com>
2025-02-05 23:09:10 -0400

    text

--- src/config/config.js
+++ src/config/config.js
@@ -4,5 +4,6 @@ dotenv.config()
 
 export const db = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER ,process.env.POSTGRES_PASSWORD, {
     host:process.env.HOST,
-    dialect:"postgres"
+    dialect:"postgres",
+    port:process.env.DB_PORT
 })

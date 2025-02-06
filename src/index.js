4eb98bdd72ec65a83c9390f46cf25b9c18263c62
lazalajochy <lazala1401jochy@outlook.com>
2025-02-05 22:55:46 -0400

    text

--- src/index.js
+++ src/index.js
@@ -29,3 +29,67 @@ console.log(process.env.DATABASE_NAME, " hello hello again")
 app.listen(3000, () => {
     console.log(`App is listening on port 3000`);
 });
+
+
+/*
+steps:
+  - name: gcr.io/cloud-builders/docker
+    env:
+      - 'HOST=${_HOST}'
+    args:
+      - build
+      - '--no-cache'
+      - '-t'
+      - >-
+        $_AR_HOSTNAME/$PROJECT_ID/cloud-run-source-deploy/$REPO_NAME/$_SERVICE_NAME:$COMMIT_SHA
+      - .
+      - '-f'
+      - Dockerfile
+      - '--build-arg=POSTGRES_DB=${_POSTGRES_DB}'
+      - '--build-arg=POSTGRES_USER=${_POSTGRES_USER}'
+      - '--build-arg=POSTGRES_PASSWORD=${_POSTGRES_PASSWORD}'
+    id: Build
+  - name: gcr.io/cloud-builders/docker
+    args:
+      - push
+      - >-
+        $_AR_HOSTNAME/$PROJECT_ID/cloud-run-source-deploy/$REPO_NAME/$_SERVICE_NAME:$COMMIT_SHA
+    id: Push
+  - name: 'gcr.io/google.com/cloudsdktool/cloud-sdk:slim'
+    args:
+      - run
+      - services
+      - update
+      - $_SERVICE_NAME
+      - '--platform=managed'
+      - >-
+        --image=$_AR_HOSTNAME/$PROJECT_ID/cloud-run-source-deploy/$REPO_NAME/$_SERVICE_NAME:$COMMIT_SHA
+      - >-
+        --labels=managed-by=gcp-cloud-build-deploy-cloud-run,commit-sha=$COMMIT_SHA,gcb-build-id=$BUILD_ID,gcb-trigger-id=$_TRIGGER_ID
+      - '--region=$_DEPLOY_REGION'
+      - '--quiet'
+      - '--set-env-vars'
+      - 'HOST=${_HOST}'
+      -
+    id: Deploy
+    entrypoint: gcloud
+images:
+  - >-
+    $_AR_HOSTNAME/$PROJECT_ID/cloud-run-source-deploy/$REPO_NAME/$_SERVICE_NAME:$COMMIT_SHA
+options:
+  substitutionOption: ALLOW_LOOSE
+  logging: CLOUD_LOGGING_ONLY
+substitutions:
+  _SERVICE_NAME: node-app
+  _DEPLOY_REGION: us-central1
+  _AR_HOSTNAME: us-central1-docker.pkg.dev
+  _TRIGGER_ID: 9d5518ed-3878-4cd3-9ed0-e0cb8a9b5676
+  _HOST: 34.46.137.174
+  _PLATFORM: managed
+tags:
+  - gcp-cloud-build-deploy-cloud-run
+  - gcp-cloud-build-deploy-cloud-run-managed
+  - node-app
+
+
+*/
